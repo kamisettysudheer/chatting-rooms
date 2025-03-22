66a9e4a083d6e3a7f753b97f5fbee7513eab149f
kamisettysudheer <183721546+kamisettysudheer@users.noreply.github.com>
2025-03-22 12:28:22 +0530

    all clients are connected to each other

--- client.js
+++ client.js
@@ -2,8 +2,13 @@ const port = Deno.args[0];
 const conn = await Deno.connect({ port });
 
 const readResponse = async (conn) => {
-  for await (const chunk of conn.readable) {
-    console.log(new TextDecoder().decode(chunk));
+  try {
+    for await (const chunk of conn.readable) {
+      const msg = new TextDecoder().decode(chunk);
+      // console.log(msg);
+    }
+  } catch {
+    console.log("disconnected the server");
   }
 };
 
@@ -12,9 +17,11 @@ const sendRequest = async (conn) => {
 
   for await (const chunk of Deno.stdin.readable) {
     const msg = new TextDecoder().decode(chunk);
+    if (msg.trim().toLowerCase() === "exit") break;
     console.log(msg);
     writer.write(chunk);
   }
+  conn.close();
 };
 
 sendRequest(conn);
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const port = Deno.args[0];
 const listener = Deno.listen({ port });
+const clients = new Map();
+let clientId = 0;
 
-const handleConnection = async (conn) => {
-  const writer = conn.writable.getWriter();
+const sendResponseToAll = (clientId, response) => {
+  clients.forEach(async (value) => {
+    const msg = `client ${clientId} : ${response}`;
+    await value.write(new TextEncoder().encode(msg));
+  });
+};
+
+const handleConnection = async (conn, clientId) => {
+  clients.set(clientId, conn);
 
   for await (const chunk of conn.readable) {
     const req = new TextDecoder().decode(chunk);
-    console.log(req);
-    writer.write(chunk);
+    // const response = `client ${clientId} : ${req}`;
+    const response = req;
+    console.log(response);
+    sendResponseToAll(clientId, response);
   }
 };
 
 for await (const conn of listener) {
-  handleConnection(conn);
+  handleConnection(conn, clientId++);
 }
